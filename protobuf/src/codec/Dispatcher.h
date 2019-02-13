640c9bd46fd039b6196b84605f8a2f53184a52c4
Edwards <330713927@qq.com>
2019-02-13 22:48:56 +0800

    1.add:新增类继承、纯虚函数、虚函数的动态绑定、抽象基类等相关注释。

--- protobuf/src/codec/Dispatcher.h
+++ protobuf/src/codec/Dispatcher.h
@@ -23,7 +23,7 @@
 typedef std::shared_ptr<google::protobuf::Message> MessagePtr;
 
 //dispatcher
-
+//不能直接创建抽象基类
 class Callback : muduo::noncopyable//含有纯虚函数的类是抽象基类，负责定义接口，后续的其他类则可以覆盖该接口
 {
 
@@ -31,7 +31,7 @@ public :
 
 	//1.每个析构函数（不加 virtual） 只负责清除自己的成员。
 	//2.可能有基类指针指向的确是派生类成员的情况。（这是很正常的）
-	//	那么当析构一个指向派生类成员的基类指针时，程序就不知道怎么办了（因为此时是静态联编）。可能只析构基类，派生类仍然存在。
+	//	那么当析构一个指向派生类成员的基类指针时，程序就是未定义（因为此时可能是静态联编）。可能只析构基类，派生类仍然存在。
 	//  如果在 派生类中 分配了 内存空间的话，没有将基类的析构函数声明为虚析构函数，很容易发生内存泄漏事件。
 	//	所以要保证运行适当的析构函数，基类中的析构函数必须为虚析构。
 	virtual ~Callback() = default;//C++11中的默认行为。对析构函数进行动态绑定
@@ -111,6 +111,7 @@ public:
 	void registerMessageCallback(const typename CallbackT<T>::ProtobufMessageTCallback& callback)
 	{
 		std::shared_ptr<CallbackT<T>> pd(new CallbackT<T>(callback));//构造一个新的模板类，并传入上层回调
+		//实参绑定的是派生类指针
 		callbacks_[T::descriptor()] = pd;//根据消息类型将类指针添加到map中
 	}
 
@@ -121,6 +122,11 @@ public:
 		CallbackMap::const_iterator it = callbacks_.find(message->GetDescriptor());
 		if (it != callbacks_.end())
 		{
+			//这里，根据引用或指针所绑定的类型不同，来决定是调用基类的版本还是派生类版本
+			//动态绑定只有在我们通过指针或引用调用虚函数时才会发生
+
+			//回避虚函数机制，强行调用基类的版本
+			//it->second->Callback::onMessage(conn, message, receiveTime);
 			it->second->onMessage(conn, message, receiveTime);//回调具体的消息类型，并回调用户回调
 		}
 		else//设置的默认回调（调试用）
@@ -135,7 +141,7 @@ private:
 	//具体的消息类型的回调map
 	//生成proto文件时，已缓存了消息格式的所有类型。
 	//每个具体的消息类型都一个全局的Descriptor对象，其地址是不变的，可以用来做key
-	//value为不同回调的智能指针
+	//value为不同回调的智能指针：类继承的动态绑定
 	typedef std::map<const google::protobuf::Descriptor*, std::shared_ptr<Callback> > CallbackMap;
 
 	CallbackMap callbacks_;
